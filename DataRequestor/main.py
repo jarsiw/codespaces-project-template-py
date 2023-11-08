ad1a1a824b528e6c43f260c4cfff101539cb4959
jarsiw <147593399+jarsiw@users.noreply.github.com>
2023-11-08 09:58:02 +0000

    zaj8.11

--- DataRequestor/main.py
+++ DataRequestor/main.py
@@ -6,9 +6,15 @@ res = requests.get("https://api.openaq.org/v2/locations/2178")
 
 #https://docs.openaq.org/docs/getting-started
 
-while (True):
-    print('Test')
-    sleep(2)
+import requests
+
+url = "https://api.openaq.org/v3/locations"
+
+headers = {"accept": "application/json"}
+
+response = requests.get(url, headers=headers)
+
+print(response.text)
 
 
 
